9eb1dcb592d69dc67fa21eb7cd6adf1860989ca5
miller2590 <miller2590@gmail.com>
2022-07-20 22:40:37 -0500

    having some issues with firebase signOut function

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -5,6 +5,7 @@ import { useAuth } from "../contexts/AuthContext";
 import { ToastContainer, toast } from "react-toastify";
 import useAuthFormState from "../hooks/useAuthFormState";
 import { useNavigate } from "react-router-dom";
+import useToggleState from "../hooks/useToggleState";
 
 function AuthForm() {
   const [
@@ -17,23 +18,36 @@ function AuthForm() {
     reset,
   ] = useAuthFormState("", "", "");
   const [error, setError] = useState("");
-  const { signUp } = useAuth();
+  const [isSignUp, toggle] = useToggleState(true);
+  const { signUp, login } = useAuth();
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (isSignUp) {
+      if (password !== confPass) {
+        return setError("Passwords do not match");
+      }
 
-    if (password !== confPass) {
-      return setError("Passwords do not match");
+      try {
+        setError("");
+        await signUp(email, password);
+        reset();
+        toggle();
+      } catch (err) {
+        toast.error(err.message);
+        reset();
+      }
     }
 
-    try {
-      setError("");
-      await signUp(email, password);
-      reset();
-    } catch (err) {
-      toast.error(err.message);
-      reset();
+    if (!isSignUp) {
+      try {
+        await login(email, password);
+        reset();
+        navigate("/home");
+      } catch (err) {
+        toast.error(err.message);
+      }
     }
   };
 
@@ -42,7 +56,9 @@ function AuthForm() {
       <ToastContainer />
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
+          <h2 className="text-center mb-4">
+            {isSignUp ? "Sign Up" : " Sign In"}
+          </h2>
           {error && <Alert variant="danger">{error}</Alert>}
 
           <Form onSubmit={handleSubmit}>
@@ -66,16 +82,21 @@ function AuthForm() {
                 required
               />
             </Form.Group>
-            <Form.Group controlId="confirm-password">
-              <Form.Label style={styles.FormLable}>Confirm Password</Form.Label>
-              <Form.Control
-                value={confPass}
-                onChange={(e) => handleConfPass(e)}
-                type="password"
-                placeholder="Confirm password"
-                required
-              />
-            </Form.Group>
+            {isSignUp && (
+              <Form.Group controlId="confirm-password">
+                <Form.Label style={styles.FormLable}>
+                  Confirm Password
+                </Form.Label>
+                <Form.Control
+                  value={confPass}
+                  onChange={(e) => handleConfPass(e)}
+                  type="password"
+                  placeholder="Confirm password"
+                  required
+                />
+              </Form.Group>
+            )}
+
             <div style={{ display: "flex", justifyContent: "center" }}>
               <Button className="w-50 mt-4" type="submit">
                 Submit
@@ -83,9 +104,13 @@ function AuthForm() {
             </div>
           </Form>
           <Card.Text className="w-100 text-center mt-2">
-            Already have an account?{" "}
-            <Button style={{ padding: "0", marginLeft: "4px" }} variant="link">
-              Sign In
+            {isSignUp ? "Already have an account?" : "Need an account?"}
+            <Button
+              onClick={toggle}
+              style={{ padding: "0", marginLeft: "4px" }}
+              variant="link"
+            >
+              {isSignUp ? "Sign In" : "Sign Up"}
             </Button>
           </Card.Text>
         </Card.Body>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { useAuth } from "../contexts/AuthContext";
+import { Button } from "react-bootstrap";
+import { ToastContainer, toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-  return <div>Home</div>;
+  const { logOut } = useAuth();
+  const navigate = useNavigate();
+
+  const handleClick = async () => {
+    try {
+      await logOut().then(navigate("/"));
+    } catch (err) {
+      toast.error(err.message);
+    }
+  };
+
+  return (
+    <div>
+      <div>Home</div>
+      <ToastContainer />
+      <Button onClick={handleClick}>Log Out</Button>
+    </div>
+  );
 }
 
 export default Home;
