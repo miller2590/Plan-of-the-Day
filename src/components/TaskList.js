fdafe29cdd378cdbb04088792011fb64175e951e
miller2590 <miller2590@gmail.com>
2023-04-07 15:49:20 -0500

    removed unused variables

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,26 +1,12 @@
 import React from "react";
 import Task from "./Task";
-import { useDrag } from "react-dnd";
-import { ItemTypes } from "../utils/Constants.js";
 
 function TaskList(props) {
-  const [{ isDragging }, drag] = useDrag(() => ({
-    type: ItemTypes.TASK,
-    collect: (monitor) => ({
-      isDragging: !!monitor.isDragging(),
-    }),
-  }));
-
   return (
     <ul style={{ listStyle: "none" }}>
       {props.taskData.map((task) => {
         return (
-          <li
-            ref={drag}
-            className="task"
-            key={task.eventKey}
-            style={{ opacity: isDragging ? 0.5 : 1 }}
-          >
+          <li className="task" key={task.eventKey}>
             <Task title={task.title} description={task.description} />
           </li>
         );
